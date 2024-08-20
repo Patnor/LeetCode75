b01e400719c79409eff8e51971caab1e1cc1ce1a
Patnor <patnor22@gmail.com>
2024-08-20 13:01:29 -0700

    did recursive version

--- IsSubsequence392.cs
+++ IsSubsequence392.cs
@@ -3,11 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace LeetCode75
 {
     internal class IsSubsequence392
     {
+        string source, target;
+        int leftBound, rightBound;
+
+        private bool rec_isSubsequence(int leftIndex, int rightIndex)
+        {
+            // base cases
+            if (leftIndex == leftBound)
+                return true;
+            if (rightIndex == rightBound)
+                return false;
+
+            // consume both strings or just the target string
+            if (source[leftIndex] == target[rightIndex])
+                ++leftIndex;
+            ++rightIndex;
+
+            return rec_isSubsequence(leftIndex, rightIndex);
+        }
+
+        public bool IsSubsequenceRec(string s, string t)
+        {
+            this.source = s;
+            this.target = t;
+            this.leftBound = s.Length;
+            this.rightBound = t.Length;
+
+            return rec_isSubsequence(0, 0);
+        }
         public static bool IsSubsequence(string s, string t)
         {
 
