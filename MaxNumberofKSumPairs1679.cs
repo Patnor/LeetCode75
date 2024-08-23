4c7290deb165b22d0d465360eee6f1ba92f90d28
Patnor <patnor22@gmail.com>
2024-08-22 18:47:12 -0700

    Got 1679 to work. beats 97% mem but only 8% time.

--- MaxNumberofKSumPairs1679.cs
+++ MaxNumberofKSumPairs1679.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,32 @@ namespace LeetCode75
 {
     static class MaxNumberofKSumPairs1679
     {
+
+        // create hashmap
+        // iterate through list checking if k - value is in hashmap. 
+        // if it is remove from hasmap, remove from array, increment output counter by 1
         public static int MaxOperations(int[] nums, int k)
         {
-            return 0;
+            List<int> list = new List<int>();
+            int val;
+            int count = 0;
+         
+            for(int i = 0; i < nums.Length; i++)
+            {
+                val = k - nums[i];
+                if(list.Contains(val))
+                {                 
+                    list.Remove(val);
+                   
+                    count++;
+                }
+                else
+                {
+                    list.Add(nums[i]);
+                }
+            }
+            nums = list.ToArray();
+            return count;
         }
     }
 }
