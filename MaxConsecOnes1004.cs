1a3aa772bc13f4e6f9f27abe288f93ab8ed1b980
Patnor <patnor22@gmail.com>
2024-08-28 15:36:52 -0700

    Finish 1004

--- MaxConsecOnes1004.cs
+++ MaxConsecOnes1004.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,36 @@ namespace LeetCode75
     {
         public static int LongestOnes(int[] nums, int k)
         {
+            int maxOnes = 0;
+            int currentOnes = 0;
+            int numZeros = k;
+            int startIndex = 0;
 
-            return 0;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 1)
+                {
+                    currentOnes++;      
+                }
+                else if (numZeros > 0) 
+                {
+                    numZeros--; 
+                    currentOnes++;                
+                }
+                else // reset
+                {
+                    maxOnes = Math.Max(currentOnes, maxOnes);
+
+                    while (nums[startIndex] != 0)
+                    {
+                        startIndex++;
+                        currentOnes--;
+                    } 
+                    startIndex++;
+                }
+            }
+            maxOnes = Math.Max(currentOnes, maxOnes);
+            return maxOnes;
         }
     }
 }
