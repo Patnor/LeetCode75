8911d9ddde135c948575927f3bd45d773858a584
Patnor <patnor22@gmail.com>
2024-08-28 16:16:06 -0700

    set up 1493. Longest Subarray of 1's After Deleting One Element

---
+++ LongestSub1493.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode75
+{
+    internal class LongestSub1493
+    {
+        public static int LongestSubarray(int[] nums)
+        {
+
+
+            return 0;
+        }
+    }
+}
