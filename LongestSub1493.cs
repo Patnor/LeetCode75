918b95a50e94cacf314635122c3d74742933751f
Patnor <patnor22@gmail.com>
2024-08-28 18:11:43 -0700

    Add alternate solutions to 1493

--- LongestSub1493.cs
+++ LongestSub1493.cs
@@ -42,5 +42,34 @@ namespace LeetCode75
             }
             return Math.Max(currentCount, maxCount);
         }
+
+        // an editorial version that uses ternary operator
+        public static int LongestSubarray2(int[] nums)
+        {
+            // Number of zero's in the window.
+            int zeroCount = 0;
+            int longestWindow = 0;
+            // Left end of the window.
+            int start = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                zeroCount += (nums[i] == 0 ? 1 : 0);
+
+                // Shrink the window until the zero counts come under the limit.
+                while (zeroCount > 1)
+                {
+                    zeroCount -= (nums[start] == 0 ? 1 : 0);
+                    start++;
+                }
+
+                longestWindow = Math.Max(longestWindow, i - start);
+            }
+
+            return longestWindow;
+        }
     }
+
+
+
 }
